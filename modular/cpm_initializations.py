e3f2421286647fbd3160b6e3968e3d9e086008a7
kav-kamath <kk1070@princeton.edu>
2025-09-30 16:12:23 -0400

    self -> cpm in cpm_initializations.py

--- modular/cpm_initializations.py
+++ modular/cpm_initializations.py
@@ -8,7 +8,7 @@ from .cpm import CPM
 ###### ALL INITILIZATIONS ######
 
 ## RANDOM ##
-def initialize_cells_random(self: CPM): #choose cell centers randomly
+def initialize_cells_random(cpm: CPM): #choose cell centers randomly
     """
     Initialize cells by placing a center randomly on the grid, then filling in a roughly cirular shape around the center.
     Repeated for each center.
@@ -21,24 +21,24 @@ def initialize_cells_random(self: CPM): #choose cell centers randomly
         None (Updates the CPM grid and updates the number of cells in case it changed.)
     """
     
-    cell_ids = range(1, self.num_cells + 1)
+    cell_ids = range(1, cpm.num_cells + 1)
 
     # new implementation: randomly choose cell centers, can totally overwrite previous cell if a cell center is
     # repeatedly chosen, code should still work but num_cells value may be lower than highest cell ID
     for cell_id in cell_ids:
-        y, x = random.randint(3, self.grid_size - 4), random.randint(3, self.grid_size - 4)
+        y, x = random.randint(3, cpm.grid_size - 4), random.randint(3, cpm.grid_size - 4)
         # main square
-        self.grid[y-2:y+3, x-2:x+3] = cell_id #[inclusive, exclusive)
+        cpm.grid[y-2:y+3, x-2:x+3] = cell_id #[inclusive, exclusive)
         #sides
-        self.grid[y-1:y+2, x-3] = cell_id # left
-        self.grid[y-3, x-1:x+2] = cell_id # top
-        self.grid[y-1:y+2, x+3] = cell_id # right
-        self.grid[y+3, x-1:x+2] = cell_id # bottom
+        cpm.grid[y-1:y+2, x-3] = cell_id # left
+        cpm.grid[y-3, x-1:x+2] = cell_id # top
+        cpm.grid[y-1:y+2, x+3] = cell_id # right
+        cpm.grid[y+3, x-1:x+2] = cell_id # bottom
 
     #get new number of cells
-    existing_cell_ids = np.unique(self.grid)
+    existing_cell_ids = np.unique(cpm.grid)
     existing_cell_ids = existing_cell_ids[existing_cell_ids != 0]
-    self.num_cells = len(existing_cell_ids)
+    cpm.num_cells = len(existing_cell_ids)
     
 
     # old implementation: always places centers on whitespace but can get stuck in while loop if no whitespace left
@@ -58,7 +58,7 @@ def initialize_cells_random(self: CPM): #choose cell centers randomly
     """
 
 ## IDEAL ##
-def initialize_cells_ideal(self: CPM): #choose cell centers such as to uniformly place cells across space
+def initialize_cells_ideal(cpm: CPM): #choose cell centers such as to uniformly place cells across space
     """
     Initialize cells to be non-overlapping and uniformly spaced across the grid, with grid filled (whitespace allowed).
 
@@ -69,20 +69,20 @@ def initialize_cells_ideal(self: CPM): #choose cell centers such as to uniformly
     """
     
     cell_id = 1
-    for y in range(3, self.grid_size - 3, 7):
-        for x in range(3, self.grid_size - 3, 7):
+    for y in range(3, cpm.grid_size - 3, 7):
+        for x in range(3, cpm.grid_size - 3, 7):
             # main sqaure
-            self.grid[y-2:y+3, x-2:x+3] = cell_id #[unclusive, exclusive]
+            cpm.grid[y-2:y+3, x-2:x+3] = cell_id #[unclusive, exclusive]
             #sides
-            self.grid[y-1:y+2, x-3] = cell_id # left
-            self.grid[y-3, x-1:x+2] = cell_id # top
-            self.grid[y-1:y+2, x+3] = cell_id # right
-            self.grid[y+3, x-1:x+2] = cell_id # bottom
+            cpm.grid[y-1:y+2, x-3] = cell_id # left
+            cpm.grid[y-3, x-1:x+2] = cell_id # top
+            cpm.grid[y-1:y+2, x+3] = cell_id # right
+            cpm.grid[y+3, x-1:x+2] = cell_id # bottom
 
             cell_id += 1
 
 ## SPACE_FILLING ##            
-def initialize_cells_space_filling(self: CPM):
+def initialize_cells_space_filling(cpm: CPM):
     """
     Initialize cells to be non-overlapping and uniformly spaced across the grid, with grid filled (whitespace NOT allowed).
 
@@ -95,25 +95,25 @@ def initialize_cells_space_filling(self: CPM):
         None (Updates the CPM grid.)
     """
     
-    initialize_cells_ideal(self) # build upon above, so cells are vaguely circular
+    initialize_cells_ideal(cpm) # build upon above, so cells are vaguely circular
     
     # iterate through all spaces, looking for empty ones
-    while 0 in self.grid:
-        for y in range(0, self.grid_size):
-            for x in range(0, self.grid_size):
+    while 0 in cpm.grid:
+        for y in range(0, cpm.grid_size):
+            for x in range(0, cpm.grid_size):
                 #if empty
-                if self.grid[y, x] == 0:
+                if cpm.grid[y, x] == 0:
                     new_cell_id = [0] # start empty list
                     for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]: #look at IDs of all neighbors
                         nx, ny = (x + dx) , (y + dy)
-                        if 0 <= nx < self.grid_size and 0 <= ny < self.grid_size: #if neighbor has valid indices
-                                if self.grid[ny, nx] != 0:
-                                    new_cell_id.append(self.grid[ny, nx])
+                        if 0 <= nx < cpm.grid_size and 0 <= ny < cpm.grid_size: #if neighbor has valid indices
+                                if cpm.grid[ny, nx] != 0:
+                                    new_cell_id.append(cpm.grid[ny, nx])
                     random_id = random.choice(new_cell_id) #choose new ID randomly
-                    self.grid[y, x] = random_id
+                    cpm.grid[y, x] = random_id
 
 ## VORONOI ##
-def initialize_cells_voronoi(self: CPM):
+def initialize_cells_voronoi(cpm: CPM):
     """
     Initialize cells by assigning pixels based on closest cell center (Voronoi tessellation).
     
@@ -129,10 +129,10 @@ def initialize_cells_voronoi(self: CPM):
 
     if center_method == "uniform": # Generate cell centers with uniform spacing
         # Calculate spacing for uniform distribution
-        num_rows = int(np.sqrt(self.num_cells))
-        num_cols = int(np.ceil(self.num_cells / num_rows))
-        spacing_y = int(self.grid_size / num_rows)  # Spacing in y direction
-        spacing_x = int(self.grid_size / num_cols)  # Spacing in x direction
+        num_rows = int(np.sqrt(cpm.num_cells))
+        num_cols = int(np.ceil(cpm.num_cells / num_rows))
+        spacing_y = int(cpm.grid_size / num_rows)  # Spacing in y direction
+        spacing_x = int(cpm.grid_size / num_cols)  # Spacing in x direction
 
         # calulcate centers
         cell_centers = []
@@ -144,21 +144,21 @@ def initialize_cells_voronoi(self: CPM):
                 x = spacing_x // 2 + j * spacing_x
                 cell_centers.append((y, x))
                 cell_id += 1
-                if cell_id > self.num_cells:
+                if cell_id > cpm.num_cells:
                     break  # Stop if we've reached the desired number of cells
-            if cell_id > self.num_cells:
+            if cell_id > cpm.num_cells:
                 break
 
     if center_method == "random": #Randomly choose cell centers:
-        cell_ids = range(1, self.num_cells + 1)
+        cell_ids = range(1, cpm.num_cells + 1)
         cell_centers = []
         for cell_id in cell_ids:
-            y, x = random.randint(0, self.grid_size - 1), random.randint(0, self.grid_size - 1) #random.randint [inclusive, inclusive]
+            y, x = random.randint(0, cpm.grid_size - 1), random.randint(0, cpm.grid_size - 1) #random.randint [inclusive, inclusive]
             cell_centers.append((y, x))
 
     #Assign pixels to cell center
-    for y in range(self.grid_size):
-        for x in range(self.grid_size):
+    for y in range(cpm.grid_size):
+        for x in range(cpm.grid_size):
             min_dist = float('inf')
             closest_cell_id = 0
             for cell_id, (cy, cx) in enumerate(cell_centers, 1):  # Start cell_id from 1
@@ -166,10 +166,10 @@ def initialize_cells_voronoi(self: CPM):
                 if dist < min_dist:
                     min_dist = dist
                     closest_cell_id = cell_id
-            self.grid[y, x] = closest_cell_id
+            cpm.grid[y, x] = closest_cell_id
 
 ## CUSTOM (customize cell centers) ##
-def initialize_cells_custom1(self: CPM, cell_centers: list[tuple[int, int]]):        
+def initialize_cells_custom1(cpm: CPM, cell_centers: list[tuple[int, int]]):        
     """
     Initialize cells with specified cell centers. Later listed cells may overlap with and overwrite formerly listed cells.
 
@@ -179,16 +179,16 @@ def initialize_cells_custom1(self: CPM, cell_centers: list[tuple[int, int]]):
         None (Updates the CPM grid and num_cells in case it changed.)
     """
     
-    assert len(cell_centers) == self.num_cells, (
-        f"{self.num_cells} cell centers expected, {len(cell_centers)} cell centers input"
+    assert len(cell_centers) == cpm.num_cells, (
+        f"{cpm.num_cells} cell centers expected, {len(cell_centers)} cell centers input"
     )
     for y, x in cell_centers:
-        assert 0 <= y < self.grid_size and 0 <= x < self.grid_size, (
+        assert 0 <= y < cpm.grid_size and 0 <= x < cpm.grid_size, (
             f"Cell center {(y, x)} is out of bounds: must be within "
-            f"(4 ≤ y < {self.grid_size - 3}, 4 ≤ x < {self.grid_size - 3})"
+            f"(4 ≤ y < {cpm.grid_size - 3}, 4 ≤ x < {cpm.grid_size - 3})"
         )
     
-    cell_ids = range(1, self.num_cells + 1)
+    cell_ids = range(1, cpm.num_cells + 1)
     
     # specify cell centers
     cell_centers = [(1,1), (8,1), (1,8), (8,8)]
@@ -197,20 +197,20 @@ def initialize_cells_custom1(self: CPM, cell_centers: list[tuple[int, int]]):
     for cell_id, (y,x) in enumerate(cell_centers, 1):
         # y, x = random.randint(3, self.grid_size - 4), random.randint(3, self.grid_size - 4)
         # main square
-        self.grid[y-2:y+3, x-2:x+3] = cell_id #[inclusive, exclusive)
+        cpm.grid[y-2:y+3, x-2:x+3] = cell_id #[inclusive, exclusive)
         #sides
-        self.grid[y-1:y+2, x-3] = cell_id # left
-        self.grid[y-3, x-1:x+2] = cell_id # top
-        self.grid[y-1:y+2, x+3] = cell_id # right
-        self.grid[y+3, x-1:x+2] = cell_id # bottom
+        cpm.grid[y-1:y+2, x-3] = cell_id # left
+        cpm.grid[y-3, x-1:x+2] = cell_id # top
+        cpm.grid[y-1:y+2, x+3] = cell_id # right
+        cpm.grid[y+3, x-1:x+2] = cell_id # bottom
 
     #get new number of cells
-    existing_cell_ids = np.unique(self.grid)
+    existing_cell_ids = np.unique(cpm.grid)
     existing_cell_ids = existing_cell_ids[existing_cell_ids != 0]
-    self.num_cells = len(existing_cell_ids)
+    cpm.num_cells = len(existing_cell_ids)
 
 ## CUSTOM (customize entire grid) ##
-def initialize_cells_custom2(self: CPM):
+def initialize_cells_custom2(cpm: CPM):
     """
     Initialize cells with hardcoded layout.
 
@@ -245,4 +245,4 @@ def initialize_cells_custom2(self: CPM):
     ]
     
     # can't directly assign self.grid (self.grid = np.array(custom_grid, dtype=int)) so need to overwrite matrix values instead
-    self.grid[0:self.grid_size, 0:self.grid_size] = np.array(custom_grid, dtype=int)
+    cpm.grid[0:cpm.grid_size, 0:cpm.grid_size] = np.array(custom_grid, dtype=int)
--- modular/hams.py
+++ modular/hams.py
@@ -7,18 +7,18 @@ from .cpm import CPM
 ###### HELPER FUNCTIONS ###### ==> FEATURES OF CELL
 
 # updated to be skimage.measure.regionprops() perimeter
-def _calculate_perimeter(self: CPM, cell_id):
+def _calculate_perimeter(cpm: CPM, cell_id):
 
     # skimage.measure.regionprops() perimeter
-    binary_grid = (self.grid == cell_id)
+    binary_grid = (cpm.grid == cell_id)
     perimeter_value = perimeter(binary_grid, neighborhood = 8)
 
     return perimeter_value
 
-def _fraction_illuminated(self: CPM, cell_id):
+def _fraction_illuminated(cpm: CPM, cell_id):
 
-    cell_mask = (self.grid == cell_id) # t/f mask of cell location
-    light_mask = (self.light_pattern == 1) # t/f mask of light location
+    cell_mask = (cpm.grid == cell_id) # t/f mask of cell location
+    light_mask = (cpm.light_pattern == 1) # t/f mask of light location
         
     overlap = cell_mask & light_mask # AND of both masks, where both true
 
@@ -29,9 +29,9 @@ def _fraction_illuminated(self: CPM, cell_id):
         return 0.0
     return area_in_light / total_area
 
-def _cell_contains_holes(self: CPM, cell_id):
+def _cell_contains_holes(cpm: CPM, cell_id):
         
-    cell_mask = (self.grid == cell_id) # binary mask for the cell
+    cell_mask = (cpm.grid == cell_id) # binary mask for the cell
     filled_mask = binary_fill_holes(cell_mask) # fill holes in the cell mask
 
     # compare original and filled masks - if equal, there are no holes
@@ -42,7 +42,7 @@ def _cell_contains_holes(self: CPM, cell_id):
 
 ###### HAMILTONIAN FUNCTION ###### ==> DIFFERENT HAMILTONIANS
     
-def calculate_hamiltonian(self: CPM):
+def calculate_hamiltonian(cpm: CPM):
     """
     Compute the total Hamiltonian energy of the current CPM grid state.
 
@@ -52,7 +52,7 @@ def calculate_hamiltonian(self: CPM):
     - Light consideration; energy of the cell is reduced if in an illuminated regions.
 
     Parameters
-        self : CPM - CPM object as defined in cpm.py
+        cpm : CPM - CPM object as defined in cpm.py
 
     Returns
         hamiltonian : float - computed Hamiltonian energy value
@@ -60,27 +60,27 @@ def calculate_hamiltonian(self: CPM):
     """
     
     hamiltonian = 0
-    cell_ids = np.unique(self.grid)
+    cell_ids = np.unique(cpm.grid)
     cell_ids = cell_ids[cell_ids != 0]
         
     for cell_id in cell_ids:
         # deltaH_ground: check for disjoint parts
-        labeled_array, num_features = label(self.grid == cell_id) # intepreter misreading the type of self.grid, totally fine at runtime
+        labeled_array, num_features = label(cpm.grid == cell_id) # intepreter misreading the type of self.grid, totally fine at runtime
         if num_features > 1:
             return np.inf  # positive infinity for disjoint parts
             
         #deltaH_ground: check for holes
-        if _cell_contains_holes(self, cell_id):
+        if _cell_contains_holes(cpm, cell_id):
             return np.inf
             
         # calc area & perimeter
-        area = np.sum(self.grid == cell_id)
-        perimeter = _calculate_perimeter(self, cell_id)
+        area = np.sum(cpm.grid == cell_id)
+        perimeter = _calculate_perimeter(cpm, cell_id)
 
         # Energy terms for area and perimeter/area ratio
-        hamiltonian += 0.2*np.abs(area - self.target_area) # deltaH_area
-        hamiltonian += 0.8*(np.abs(((area**(1/2)) / perimeter) - self.target_ratio)) # deltaH_area/perimeter_ratio
-        hamiltonian -= _fraction_illuminated(self, cell_id)  # no specific deltaH term as outlined in JP, but deltaH_lum for now
+        hamiltonian += 0.2*np.abs(area - cpm.target_area) # deltaH_area
+        hamiltonian += 0.8*(np.abs(((area**(1/2)) / perimeter) - cpm.target_ratio)) # deltaH_area/perimeter_ratio
+        hamiltonian -= _fraction_illuminated(cpm, cell_id)  # no specific deltaH term as outlined in JP, but deltaH_lum for now
 
     return hamiltonian
 
