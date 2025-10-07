d86e8f3e1f9cec08bc8db7c07f44a42bf04091d7
kav-kamath <kk1070@princeton.edu>
2025-10-07 03:48:36 -0400

    cleaning loop formatting in mc step function + updating hams functions to use regionprops

--- modular/monte_carlo_step.py
+++ modular/monte_carlo_step.py
@@ -13,8 +13,39 @@ def monte_carlo_step(cpm: CPM):
         None (Updates CPM grid and mc_step in place.)
 
     """
-    
-    
+    current_hamiltonian = hams.calculate_hamiltonian(cpm)
+
+    for _ in range(cpm.grid_size**2):  # N random grid points
+        i_x, i_y = random.randint(0, cpm.grid_size - 1), random.randint(0, cpm.grid_size - 1)
+        dx, dy = random.choice([(1, 0), (-1, 0), (0, 1), (0, -1)])
+        j_x, j_y = (i_x + dx), (i_y + dy)
+
+        # if jx,jy is a valid grid point (no wrapping around)
+        if not (0 <= j_x < cpm.grid_size) & (0 <= j_y < cpm.grid_size):
+            continue
+
+        #if xi,xj and jx,jy have different cell IDs
+        if  not (cpm.grid[i_y, i_x] != cpm.grid[j_y, j_x]):
+            continue
+
+        # change j to i, calculate new hamiltonian
+        old_j_value = cpm.grid[j_y, j_x]
+        cpm.grid[j_y, j_x] = cpm.grid[i_y, i_x]
+        new_hamiltonian = hams.calculate_hamiltonian(cpm)
+
+        # deltaH
+        delta_hamiltonian = new_hamiltonian - current_hamiltonian
+
+        if (delta_hamiltonian <= 0) or (random.random() < np.exp(-delta_hamiltonian / cpm.temperature)):
+            current_hamiltonian = new_hamiltonian
+
+        else:
+            cpm.grid[j_y, j_x] = old_j_value  # reject j -> i
+
+    cpm.mc_step += 1  # increment time by 1 every time one full monte carlo step is complete (all N events have been attempted)
+
+    # old implementation: pick N random grid points, attempt copy for each    
+    """
     for _ in range(cpm.grid_size**2):  # N random grid points
         i_x, i_y = random.randint(0, cpm.grid_size - 1), random.randint(0, cpm.grid_size - 1)
         dx, dy = random.choice([(1, 0), (-1, 0), (0, 1), (0, -1)])
@@ -40,7 +71,9 @@ def monte_carlo_step(cpm: CPM):
                     pass  # accept j -> i
                 else:
                     cpm.grid[j_y, j_x] = old_j_value  # reject j -> i
+                    
     cpm.mc_step += 1 #increment time by 1 every time one full monte carlo step is complete (all N events have been attempted)
+    """
         
 def mc_sim(cpm, num_steps):
     """
