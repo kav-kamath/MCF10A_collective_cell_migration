44f8295d2ad7a580ea6e32efa8c9dbd46d368e0f
kav-kamath <kk1070@princeton.edu>
2025-11-04 04:28:40 -0500

    get .ipynb filw working w new structure

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 import numpy as np
 import random
 import os
+
 from mcf10amigration.cpm_initializations import *
 
 # my functions
 
 from mcf10amigration import *
 
-grid_size = 28
+grid_size = 56
 num_cells = 5 #relevant if using initialize_cells_random or initiliaze_cells_voronoi when constructing CPM
 target_area = 37 #49 #37
 target_perimeter = 20.992 #20.992 #0.00
@@ -23,7 +24,7 @@ light_function = light.multiple_moving_bars_light
 cpm = CPM(grid_size, num_cells, target_area, target_perimeter, target_ratio, temperature, initialize_cells_ideal, light_function)
 
 #frames_for_plot, event_times = gillespie_sim(cpm, max_time=0.008)
-frames_for_plot, light_patterns, event_times = mc_sim(cpm, num_steps=5)
+frames_for_plot, light_patterns, event_times = mc_sim(cpm, num_steps=50)
 
 #output_path = os.path.join(output_folder, f"calc_simulation.mp4")
 animate_simulation(frames_for_plot, event_times, output_filename="current_simulation.mp4")
