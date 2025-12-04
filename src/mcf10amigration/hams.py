3585fb790b9ac1130139518350f36f82ab884407
kav-kamath <kk1070@princeton.edu>
2025-12-04 12:27:10 -0500

    roundedness

--- src/mcf10amigration/hams.py
+++ src/mcf10amigration/hams.py
@@ -100,7 +100,8 @@ def calculate_hamiltonian(cpm: CPM):
         return np.inf
 
     hamiltonian += np.sum(np.power(np.abs(props_table['area'] - cpm.target_area), 2))
-    hamiltonian += np.sum(np.power(np.abs(props_table['perimeter_crofton'] - cpm.target_perimeter), 4))
+    hamiltonian += np.sum(np.power(np.abs(props_table['perimeter_crofton'] - (2*np.pi*np.sqrt((props_table['area']/np.pi))) ), 4))
+    # hamiltonian += np.sum(np.power(np.abs(props_table['perimeter_crofton'] - cpm.target_perimeter), 4))
     hamiltonian -= np.sum(np.power(100 * np.abs(props_table['intensity_mean']), cpm.k)) #1.75 #3.5
 
     return hamiltonian
